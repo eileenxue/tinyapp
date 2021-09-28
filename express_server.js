591cfb2e4c03b33d4f50c5d8de8425d87c733dc4
Eileen Xue <xue.eileen@gmail.com>
2021-09-28 03:21:15 +0000

    Added redirection of shortURLs to longURLs

--- express_server.js
+++ express_server.js
@@ -26,39 +26,33 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
- 
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
-
 // change from url to urls
- app.get('/urls', (req, res) => {
-   const templateVars = { urls: urlDatabase };
-   res.render("urls_index", templateVars)
- })
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars)
+})
  
- app.get("/urls/new", (req, res) => {
-   res.render("urls_new");
- });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
- app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+// Redirect the shortened URL to original longURL
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
   // res.send("Ok");  // Respond with 'Ok' (we will replace this)
   // res.status = 200;
-  
+
   // Generate shortURL and then log that and longURL to urlDatabase
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
