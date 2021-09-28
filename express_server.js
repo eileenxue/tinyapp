9529b55faeefb18488958b171560e74289ee7741
Eileen Xue <xue.eileen@gmail.com>
2021-09-28 03:08:48 +0000

    Store shortURL and longURL to urlDatabase and redirect page after form submission

--- express_server.js
+++ express_server.js
@@ -13,7 +13,6 @@ function generateRandomString() {
   return string;
 }
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -50,15 +49,25 @@ app.get("/urls.json", (req, res) => {
    res.render("urls_new");
  });
 
- app.post("/urls", (req, res) => {
-   console.log(req.body);  // Log the POST request body to the console
-   res.send("Ok");         // Respond with 'Ok' (we will replace this)
- });
-
  app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  // res.send("Ok");  // Respond with 'Ok' (we will replace this)
+  // res.status = 200;
+  
+  // Generate shortURL and then log that and longURL to urlDatabase
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+
+  // Redirect page to shortURL
+  res.redirect(`/urls/${shortURL}`);
+
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
