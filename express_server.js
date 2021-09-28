6e9cf4189bad2d2baabc39f1f591d0ab671c803e
Eileen Xue <xue.eileen@gmail.com>
2021-09-28 01:50:34 +0000

    Added create new tinyURL and generate random string function

--- express_server.js
+++ express_server.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+// Simulate unique shortURL by returning a string of 6 random alphanumeric characters
+function generateRandomString() {
+  let string = Math.random().toString(36).slice(2, 8);
+  return string;
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,23 +27,33 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+//  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+//  app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+//  });
 
- app.get('/url', (req, res) => {
+// change from url to urls
+ app.get('/urls', (req, res) => {
    const templateVars = { urls: urlDatabase };
    res.render("urls_index", templateVars)
  })
+ 
+ app.get("/urls/new", (req, res) => {
+   res.render("urls_new");
+ });
+
+ app.post("/urls", (req, res) => {
+   console.log(req.body);  // Log the POST request body to the console
+   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+ });
 
  app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
